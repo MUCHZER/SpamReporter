bfe076117a24790c09cdd123d9b3c3ea77a891d7
GRAND Léo <leograndcontact@gmail.com>
2016-08-23 15:15:46 +0200

    Fixed merge once again lol

--- index.php
+++ index.php
@@ -44,15 +44,8 @@ $router->map('POST','/comment/post/', function(  ){
     include_once 'controller/Controller.php';
 });
 
-$router->map('GET|POST','/vote/[a:view].[a:format]?', function( $view, $format ){
+$router->map('GET|POST','/vote/', function( ){
     $method = "vote";
-    $arg['vote'] = $_REQUEST['vote'];
-    //$arg['author_token'] = $_REQUEST['author_token'];
-    $arg['author_token'] = $_REQUEST[1];
-    // $arg['post_id'] = $_REQUEST['post_id'];
-    $arg['post_id'] = $_REQUEST[80];
-    $arg['format'] = $format;
-    $arg['view'] = $view;
     include_once 'controller/Controller.php';
 });
 
@@ -68,7 +61,7 @@ $router->map('POST','/search/', function(){
     $method = "search";
     $arg['term'] =  $_REQUEST['search'] ;
     $arg['format'] = 'html';
-    $arg['view'] = 'index';
+    $arg['view'] = 'searchResult';
     include_once 'controller/Controller.php';
 });
 
